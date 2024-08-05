4b12ee5e30a6b2802eeb25f18c9402df10b98b3f
zzzzDenzzzz <12den24@gmail.com>
2024-08-05 20:57:46 +0300

    fix post

--- app/src/main/java/com/example/app_7/Post.java
+++ app/src/main/java/com/example/app_7/Post.java
@@ -1,18 +1,9 @@
 package com.example.app_7;
 
 public class Post {
-    private int userId;
-    private int id;
+
     private String title;
     private String body;
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public int getId() {
-        return id;
-    }
 
     public String getTitle() {
         return title;
